6630252b080db8dd91a8c12fd148cf6970433ad4
Yunhyunjo <gotqlc147@likelion.org>
2021-11-18 19:26:09 +0900

    CommentReport

--- src/main/java/ion/kosa/TazanBack/controller/commentController.java
+++ src/main/java/ion/kosa/TazanBack/controller/commentController.java
@@ -2,11 +2,13 @@ package ion.kosa.TazanBack.controller;
 
 import ion.kosa.TazanBack.Service.commentServiceImpl;
 import ion.kosa.TazanBack.VO.commentAllVO;
+import ion.kosa.TazanBack.VO.commentReportVO;
 import ion.kosa.TazanBack.VO.commentVO;
 import ion.kosa.TazanBack.model.dto.UserRoleDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
@@ -43,5 +45,15 @@ public class commentController {
     List<commentAllVO> selectAllComment(@RequestHeader(value="Auth", required = false) String userRole){
         if(userRole == null) return null;
         return commentService.selectAllComment();
+    }
+
+    @GetMapping("/comment/report/update/{commentID}/{userID}")
+    public void updateReportCount(@PathVariable int commentID, @PathVariable int userID){
+        commentReportVO commentReportVO = new commentReportVO();
+        commentReportVO.setCommentID(commentID);
+        commentReportVO.setUserID(userID);
+        commentReportVO.setReportDate(new Date());
+        commentService.insertReportFlag(commentReportVO);
+        commentService.updateReportCount(commentID);
     }
 }
