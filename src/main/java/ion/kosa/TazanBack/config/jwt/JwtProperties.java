49f9d49e6560655f6ce53ae5e8d1614f56d865ee
qkrtndh <qkrtndh5@naver.com>
2021-11-18 10:50:59 +0900

    token modify

--- src/main/java/ion/kosa/TazanBack/config/jwt/JwtProperties.java
+++ src/main/java/ion/kosa/TazanBack/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package ion.kosa.TazanBack.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "TaZan";
-    int EXPIRATION_TIME=10000;//1000 = 1초 10000 = 10초 60000 = 1분 *10 = 10분
+    int EXPIRATION_TIME=1000000000;//1000 = 1초 10000 = 10초 60000 = 1분 *10 = 10분
     String TOKEN_PREFIX="Bearer ";
     String HEADER_STRING="Authorization";
 }
